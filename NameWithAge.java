fdb2f7fd82ec0fed6c601762de636b9719fc8828
Chiquee <almamae315@gmail.com>
2025-01-28 16:14:32 +0800

    Add files via upload

---
+++ NameWithAge.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name.with.age;
+import java.util.Scanner;
+/**
+ *
+ * @author Student
+ */
+public class NameWithAge {
+ public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in); 
+    
+    System.out.print("Hello My name is Alma Mae A. Concepcion ");
+    
+     System.out.print("Enter your age: ")
+    int age = scanner.nextInt();
+   
+    System.out.print("Hello my name is Alma Mae A. Concepion and I am " + age + " years old")
+}
+    /**
+     * @param args the command line arguments
+   
+    
+}
